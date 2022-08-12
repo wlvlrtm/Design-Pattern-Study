8a527709d5b2c77cb6b531a0fd5b9ab83c592917
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2022-08-12 15:48:27 +0900

    Observer Pattern Add

---
+++ src/P9/Observer.java
@@ -0,0 +1,5 @@
+package P9;
+
+public interface Observer {
+    public abstract void update();
+}
---
+++ src/P9/ScoreRecord.java
@@ -0,0 +1,18 @@
+package P9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScoreRecord extends Subject {
+    private List<Integer> scores = new ArrayList<Integer>();
+
+
+    public void addScore(int score) {
+        scores.add(score);
+        notifyObservers();
+    }
+
+    public List<Integer> getScoreRecord() {
+        return scores;
+    }
+}
